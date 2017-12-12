bed8f2621c81f0dd648919659a6aead7276aa052
kylerichetti <kyle.richetti@gmail.com>
2017-12-11 21:50:38 -0700

    Added User Authentication
    BrandsController now checks token roles  for permissions

--- v1/src/Controllers/BrandsController.php
+++ v1/src/Controllers/BrandsController.php
@@ -16,93 +16,140 @@ class BrandsController
 {
     //Get a single brand by ID
     public function getBrandByID($brandID){
-        $brand = new Brand($brandID);
+        $role = Token::getRoleFromToken();
+        if($role == Token::ROLE_DEV || $role == Token::ROLE_ADMIN) {
+            $brand = new Brand($brandID);
 
-        if($brand->getBrandName() == -1){
-            http_response_code(StatusCodes::BAD_REQUEST);
-            die("Brand not found");
+            if ($brand->getBrandName() == -1) {
+                http_response_code(StatusCodes::BAD_REQUEST);
+                die("Brand not found");
+            }
+            return $brand;
         }
-
-        return $brand;
+        else{
+            //unauthorized
+            http_response_code(StatusCodes::UNAUTHORIZED);
+            die("No token provided");
+        }
     }
     //Get all active brands
     public function getAllBrands()
     {
-        $brands = (new Brand())->getAllBrands();
-        if($brands == -1)
-        {
-            //Error, no brands in database
-            http_response_code(StatusCodes::BAD_REQUEST);
-            die("No brands in database");
+        $role = Token::getRoleFromToken();
+        if($role == Token::ROLE_DEV || $role == Token::ROLE_ADMIN) {
+            $brands = (new Brand())->getAllBrands();
+            if ($brands == -1) {
+                //Error, no brands in database
+                http_response_code(StatusCodes::BAD_REQUEST);
+                die("No brands in database");
+            }
+            return $brands;
         }
-        return $brands;
+        else{
+            //unauthorized
+            http_response_code(StatusCodes::UNAUTHORIZED);
+            die("No token provided");
+        }
     }
     //Create a brand
     public function createBrand($newBrandData){
-        //Permissions test
+        $role = Token::getRoleFromToken();
+        if($role == Token::ROLE_ADMIN) {
 
-        $brand = new Brand(0);
-        $newBrandName = $newBrandData['brandName'];
+            $brand = new Brand(0);
+            $newBrandName = $newBrandData['brandName'];
 
-        //Try to set name of the new brand
-        if($brand->setBrandName($newBrandName) == -1){
-            //Error, tried to pass bad data as a brand name
-            http_response_code(StatusCodes::BAD_REQUEST);
-            die("Invalid brand name");
+            //Try to set name of the new brand
+            if ($brand->setBrandName($newBrandName) == -1) {
+                //Error, tried to pass bad data as a brand name
+                http_response_code(StatusCodes::BAD_REQUEST);
+                die("Invalid brand name");
+            }
+
+            //Try to insert new brand into database
+            if ($brand->createBrand()) {
+                //Insert succeeded
+                http_response_code(StatusCodes::CREATED);
+                return $brand->JsonSerialize();
+            } else {
+                //Insert failed
+                //TODO: Double check proper response code
+                http_response_code(StatusCodes::BAD_REQUEST);
+                die("Insert failed");
+            }
         }
-
-        //Try to insert new brand into database
-        if($brand->createBrand()){
-            //Insert succeeded
-            http_response_code(StatusCodes::CREATED);
-            return $brand->JsonSerialize();
+        elseif ($role == Token::ROLE_DEV){
+            //Forbidden
+            http_response_code(StatusCodes::FORBIDDEN);
+            die("Improper Role");
         }
         else{
-            //Insert failed
-            //TODO: Double check proper response code
-            http_response_code(StatusCodes::BAD_REQUEST);
-            die("Insert failed");
+            //unauthorized
+            http_response_code(StatusCodes::UNAUTHORIZED);
+            die("No token provided");
         }
     }
     //Update a brand
     public function updateBrand($brandID, $updatedBrandData){
         //Permissions test
+        $role = Token::getRoleFromToken();
+        if($role == Token::ROLE_ADMIN) {
 
-        $brand = new Brand($brandID);
-        $newBrandName = $updatedBrandData['brandName'];
+            $brand = new Brand($brandID);
+            $newBrandName = $updatedBrandData['brandName'];
 
-        //Check if brand exists
-        if($brand->getBrandName() == -1){
-            http_response_code(StatusCodes::BAD_REQUEST);
-            die("Brand not found");
+            //Check if brand exists
+            if ($brand->getBrandName() == -1) {
+                http_response_code(StatusCodes::BAD_REQUEST);
+                die("Brand not found");
+            }
+
+            //Update database
+            if ($brand->updateBrand($newBrandName)) {
+                http_response_code(StatusCodes::OK);
+                return $brand->JsonSerialize();
+            } else {
+                //Response code?
+                http_response_code(StatusCodes::NOT_MODIFIED);
+                die("Update failed");
+            }
         }
-
-        //Update database
-        if($brand->updateBrand($newBrandName))
-        {
-            http_response_code(StatusCodes::OK);
-            return $brand->JsonSerialize();
+        elseif ($role == Token::ROLE_DEV){
+            //Forbidden
+            http_response_code(StatusCodes::FORBIDDEN);
+            die("Improper Role");
         }
         else{
-            //Response code?
-            http_response_code(StatusCodes::NOT_MODIFIED);
-            die("Update failed");
+            //unauthorized
+            http_response_code(StatusCodes::UNAUTHORIZED);
+            die("No token provided");
         }
     }
     //Soft delete a brand
     public function deleteBrand($brandID){
         //Permissions test
-
-        $brand = new Brand($brandID);
-        if($brand->deleteBrand() == 1){
-            //Response code?
-            http_response_code(StatusCodes::OK);
-            return "Success";
+        $role = Token::getRoleFromToken();
+        if($role == Token::ROLE_ADMIN) {
+            $brand = new Brand($brandID);
+            if ($brand->deleteBrand() == 1) {
+                //Response code?
+                http_response_code(StatusCodes::OK);
+                return "Success";
+            } else {
+                //Response code?
+                http_response_code(StatusCodes::NOT_MODIFIED);
+                die("Delete failed");
+            }
+        }
+        elseif ($role == Token::ROLE_DEV){
+            //Forbidden
+            http_response_code(StatusCodes::FORBIDDEN);
+            die("Improper Role");
         }
         else{
-            //Response code?
-            http_response_code(StatusCodes::NOT_MODIFIED);
-            die("Delete failed");
+            //unauthorized
+            http_response_code(StatusCodes::UNAUTHORIZED);
+            die("No token provided");
         }
     }
 }
